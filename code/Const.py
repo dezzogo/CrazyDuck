4d6542e0af5bbd8cabef2fcd31b8f55a6f04d951
dezzogo <dezzogo@gmail.com>
2025-07-14 21:27:37 -0300

    Creating asset folder Inserting MenuBG and Menu mp3 on asset Creating menu: background, sound and fonts Creating const files Having difficulties to run game window: by trying, nothing happens and process finishes. Working on it.

---
+++ code/Const.py
@@ -0,0 +1,12 @@
+# C
+COLOR_NEON_ORANGE = (255, 95, 31)
+COLOR_DEEP_PINK = (255,20,147)
+
+# M
+MENU_OPTION = ('NEW GAME',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
